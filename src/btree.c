865725929a64d596dc057eb3ec3d9b11a8d49bd0
davidattip <138945499+davidattip@users.noreply.github.com>
2024-10-14 12:47:12 +0200

    impléméntation de mon arbre binaire

--- src/btree.c
+++ src/btree.c
@@ -3,3 +3,16 @@
 void initBTree() {
     printf("Binary tree initialized.\n");
 }
+
+typedef struct Node {
+    int key;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int key) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
